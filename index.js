caa83c24160fa81853a06ad5005ca233850f6759
Pedro Henrique <pedro_rike@hotmail.com.br>
2021-11-30 16:44:45 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const uuid = require('uuid')
+const cors = require('cors')
+
+const port = 3001
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+const pedidos = []
+
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = pedidos.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ message: 'User not found' })
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next()
+
+}
+
+
+app.get('/pedidos', (request, response) => {
+
+    return response.json(pedidos)
+})
+
+
+app.post('/pedidos', (request, response) => {
+
+    const { mesa } = request.body
+
+    const pedido = { id: uuid.v4(), mesa }
+
+    pedidos.push(pedido)
+
+    return response.status(201).json(pedido)
+})
+
+
+app.delete('/pedidos/:id', checkUserId, (request, response) => {
+
+    const index = request.userIndex
+
+    pedidos.splice(index, 1)
+
+    return response.status(204).json()
+})
+
+
+app.listen(port, () => {
+    console.log(`Serve started ${port}`)
+})
