ee9aec5b1d1259f2069aa1cbe3bc83f236fc0ff6
Pedro Henrique <pedro_rike@hotmail.com.br>
2021-12-01 11:42:58 -0300

    Ajuste index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const uuid = require('uuid')
 const cors = require('cors')
 
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 app.use(cors())
